f88e961a0a63213b98509b04212c7ad698f489a7
Ege Ünalan <egeunalan@gmail.com>
2023-05-24 03:03:55 +0300

    Add Ranger21 optimizer from https://github.com/lessw2020/Ranger21

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from PIL import Image
 from torch.optim import Optimizer
 import gradio as gr
 
+from ranger import Ranger21
+
 os.sys.path.append(os.getcwd() +'/stylegan3')
 
 optimizers = {"Adam": torch.optim.Adam,
-              "SGD": torch.optim.SGD}
+              "SGD": torch.optim.SGD,
+              "Ranger21": Ranger21}
 
 
 # Using the slightly modified loss and optimizer implementations from
@@ -111,18 +114,28 @@ def generate(img, optimizer, ds_method, ds_size, lr, steps,
     
     ref_32 = ref_32.detach()
     latent = torch.randn((batch_size, 14, 512), dtype=torch.float, requires_grad=True, device='cuda')
-
-    optim = SphericalOptimizer(optimizers[optimizer], [latent], lr=lr)
+    #z = torch.from_numpy(np.random.randn(batch_size, G.z_dim)).cuda()
+    #latent2 = G.mapping(z, None, truncation_psi=0.5, truncation_cutoff=8)
+    #latent = G.mapping(z, None, truncation_psi=0.5, truncation_cutoff=8)
+    #latent.requires_grad = True
+    #latent = torch.tensor((0.9 * latent.detach() + 0.1 * latent2.detach()), dtype=torch.float, requires_grad=True, device='cuda')
+    #optim = SphericalOptimizer(optimizers[optimizer], [latent], lr=lr)
+    opt_settings = {"lr": lr}
+    if optimizer == "Ranger21":
+        opt_settings["num_epochs"] = steps
+        opt_settings["num_batches_per_epoch"] = batch_size
+    optim = SphericalOptimizer(optimizers[optimizer], [latent], **opt_settings)
+    #optim = SphericalOptimizer(Ranger21, [latent], lr=lr, num_epochs=steps, num_batches_per_epoch=batch_size)
 
     # Schedulers from https://github.com/adamian98/pulse
-    lr_schedule = 'linear1cycledrop'
-    schedule_dict = {
-                'fixed': lambda x: 1,
-                'linear1cycle': lambda x: (9*(1-np.abs(x/steps-1/2)*2)+1)/10,
-                'linear1cycledrop': lambda x: (9*(1-np.abs(x/(0.9*steps)-1/2)*2)+1)/10 if x < 0.9*steps else 1/10 + (x-0.9*steps)/(0.1*steps)*(1/1000-1/10),
-            }
-    schedule_func = schedule_dict[lr_schedule]
-    scheduler = torch.optim.lr_scheduler.LambdaLR(optim.opt, schedule_func)
+    #lr_schedule = 'linear1cycledrop'
+    #schedule_dict = {
+    #            'fixed': lambda x: 1,
+    #            'linear1cycle': lambda x: (9*(1-np.abs(x/steps-1/2)*2)+1)/10,
+    #            'linear1cycledrop': lambda x: (9*(1-np.abs(x/(0.9*steps)-1/2)*2)+1)/10 if x < 0.9*steps else 1/10 + (x-0.9*steps)/(0.1*steps)*(1/1000-1/10),
+    #        }
+    #schedule_func = schedule_dict[lr_schedule]
+    #scheduler = torch.optim.lr_scheduler.LambdaLR(optim.opt, schedule_func)
 
     min_loss = [np.inf] * batch_size
     best = [None] * batch_size
@@ -131,20 +144,21 @@ def generate(img, optimizer, ds_method, ds_size, lr, steps,
         optim.opt.zero_grad()
         for b in range(batch_size):
             w = leaky(latent[None, b] * gaussian_fit["std"] + gaussian_fit["mean"])
-            #img = (G.synthesis(w, noise_mode='random', force_fp32=True) + 1) / 2 # NCHW, float32, dynamic range [-1, +1], no truncation
-            img = (G.synthesis(w, noise_mode='none', force_fp32=True) + 1) / 2 # NCHW, float32, dynamic range [-1, +1], no truncation
+            img = (G.synthesis(w, noise_mode='random', force_fp32=True) + 1) / 2 # NCHW, float32, dynamic range [-1, +1], no truncation
+            #img = (G.synthesis(w, noise_mode='none', force_fp32=True) + 1) / 2 # NCHW, float32, dynamic range [-1, +1], no truncation
             img = img.clamp(0, 1)
             img_32 = torch.nn.functional.interpolate(img, size=ds_size, mode=ds_method)
             img_32 = img_32.clamp(0, 1)
             l = loss(w, img_32, ref_32, c_l2, c_ld)
             print(l, end='\r')
             if (l < min_loss[b]):
-                min_loss[b] = l.detach()
-                best[b] = img.clone().detach()
-                best_32[b] = img_32.clone().detach()
+                with torch.no_grad():
+                    min_loss[b] = l.detach()
+                    best[b] = img.clone().detach()
+                    best_32[b] = img_32.clone().detach()
             l.backward()
         optim.step()
-        scheduler.step()
+    #    scheduler.step()
 
     best_32 = [x.cpu().numpy().transpose((0, 2, 3, 1))[0] for x in best_32]
     best = [x.cpu().numpy().transpose((0, 2, 3, 1))[0] for x in best]
@@ -190,7 +204,7 @@ with gr.Blocks(css=css) as app:
                     ds_size = gr.Slider(2, 256, value=32, label="Downsampled Size NxN",
                                         info="Downsamples the image to size NxN if it is larger than NxN")
                 with gr.Row():
-                    opt = gr.Dropdown(["Adam", "SGD"], value="Adam", label="Optimizer")
+                    opt = gr.Dropdown(["Adam", "SGD", "Ranger21"], value="Adam", label="Optimizer")
                     lr = gr.Slider(0, 2, value=0.4, label="Learning Rate")
                     steps = gr.Slider(1, 800, value=200, label="Optimization Steps")
                     batch_size = gr.Slider(1, 16, value=1, step=1, label="Batch Size")
@@ -222,7 +236,7 @@ with gr.Blocks(css=css) as app:
                     ds_size = gr.Slider(2, 256, value=32, label="Downsampled Size NxN",
                                         info="Downsamples the image to size NxN if it is larger than NxN")
                 with gr.Row():
-                    opt = gr.Dropdown(["Adam", "SGD"], value="Adam", label="Optimizer")
+                    opt = gr.Dropdown(["Adam", "SGD", "Ranger21"], value="Adam", label="Optimizer")
                     lr = gr.Slider(0, 2, value=0.4, label="Learning Rate")
                     steps = gr.Slider(1, 800, value=200, label="Optimization Steps")
                     batch_size = gr.Slider(1, 16, value=1, step=1, label="Batch Size")
